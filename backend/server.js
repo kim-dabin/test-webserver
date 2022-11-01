0cc53e3999cdba0d93b8581959a25523be022af3
kim-dabin <pippisgoing@gmail.com>
2022-11-01 15:27:00 +0900

    backend frontend 나눔

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-const server = app.listen(port, function () {
+app.listen(port, function () {
     console.log("Express server has started on port : "+port);
 });
 
-app.get('/', function (req, res) {
-    res.send('Hello Nodemon');
-});
